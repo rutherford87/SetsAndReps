e177ad64ebfa7bee40fd419fc035fe496c288eb4
Tyler Rutherford <rutherford87@gmail.com>
2021-05-17 22:42:49 -0500

    added remaining CRUD operators to api.js. Troubleshooting success of additions

--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,77 @@ const router = require('express').Router();
 const mongoose = require("mongoose");
 const Workout = require("../models/workout");
 
+router.post('/api/workouts', ({body}, res) =>{
+  Workout.create({body})
+  .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  
+})
 
-router.get('/api/workouts', (req, res) => {
-    Workout.find().then((results) => {
-      res.json(results);
-    })  
-      .catch((error)=> {
-        res.json(error);
-      })
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((results) => {
+      res.json(results)
+    })
+    .catch((error) => {
+      res.json(error);
+    });
 });
 
-module.exports = router;
+router.get('/api/workouts', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: 'exercises.duration'
+        },
+      },
+    },
+  ])
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+  });
+
+router.get('/api/workouts/range', (req, res) => {
+  Workout.aggregate([
+{
+  $addFields:{
+    totalDuration:{
+      $sum:'exercises.duration'
+    },
+  },
+},
+  ])
+  .sort({_id:-1})
+  .limit(7)
+  .then((results) => {
+    res.json(results)
+  })
+  .catch((error)=> {
+    res.json(error);
+});
+});
+
+router.delete('/api/workouts', ({body}, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+
+module.exports = router
