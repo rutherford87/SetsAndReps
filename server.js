0040f22a5fbcfb42119b4115b7abda700eb1e4ce
Tyler Rutherford <rutherford87@gmail.com>
2021-05-18 22:20:32 -0500

    udpated mongoose connection. CRUD working

--- server.js
+++ server.js
@@ -15,8 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"))
 
-//mongoose.connect("mongodb://localhost/workout")
-
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 // routes
 app.use(require("./routes/api.js"));
 
